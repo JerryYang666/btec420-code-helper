939700ba5be14d7946e89cadd07e81993bca4887
Ruihuang Yang <rxy216@case.edu>
2025-11-05 20:02:45 -0500

    Update site configuration to reflect new branding as "BTEC420 Code Helper" and change GitHub link to the new repository.

--- client/config/site.ts
+++ client/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
+  name: "BTEC420 Code Helper",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
@@ -24,10 +24,6 @@ export const siteConfig = {
     },
   ],
   links: {
-    github: "https://github.com/heroui-inc/heroui",
-    twitter: "https://twitter.com/hero_ui",
-    docs: "https://heroui.com",
-    discord: "https://discord.gg/9b6yyZKmH4",
-    sponsor: "https://patreon.com/jrgarciadev",
+    github: "https://github.com/JerryYang666/btec420-code-helper",
   },
 };
